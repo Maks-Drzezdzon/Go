4f080c669addfddd545feed7138dc33b6d815cf1
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-04-11 16:04:27 +0100

    looking over docs

--- Leetcode/Test.go
+++ Leetcode/Test.go
@@ -2,34 +2,45 @@ package main
 
 import (
 	"fmt"
-	"testing"
 )
+
+/*
+unused code functions will case errors in go
+
+# pay attention to methods too #
+lowercase var is private
+- name
+
+uppercase var is public
+- Name
+
+blockcase var is block, not visable outside of block of code
+- NAME
+
+keep acronyms uppercase
+- dbHTTP over dbHttp
+- dbURL over dbUrl
+
+*/
 
 var (
 	// init new vars and shows they can be related but that is a design decision
 	name     string = "maks"
-	lastName string = "d"
-	age      int    = 24
+	lastName        = "d"
+	age      int    = 20
+	// cant use := in global variable scopes
 )
 
 func main() {
 	/* This is my first sample program. */
-	Hello(name)
+	hello(name)
 	//TestHello()
-	// for types use %T
-	fmt.Printf("%T", age)
+	// for types use %T and %v for value
+	age := 24
+	fmt.Printf("%v is now a %T and was an %T", age, float32(age), age)
 
 }
 
-func Hello(data string) string {
+func hello(data string) string {
 	return data
-}
-
-func TestHello(t *testing.T) {
-	got := Hello("")
-	want := "Hello, world"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
 }
