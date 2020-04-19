516f90c5ec44f8093d8f6f717ceb43a8f5811109
maksthorn <maksdrzezdzon@gmail.com>
2020-04-19 13:17:58 +0100

    adding struct and setting up mvc

--- Go/main.go
+++ Go/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
 
+	"github.com/pluralsight/webservice/Go/models"
+)
+
+func main() error {
+	u := models.User{ID: 2, FirstName: "m", LastName: "d"}
+	fmt.Println(u)
+	return errors.New("failed to run main")
 }
