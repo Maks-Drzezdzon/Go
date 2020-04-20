14dcc721cfb97a007ba5fdcdb9e0b823b095a918
maksthorn <maksdrzezdzon@gmail.com>
2020-04-20 15:09:09 +0100

    adding notes

--- Go/main.go
+++ Go/main.go
@@ -15,6 +15,9 @@ func main() {
 	// https://blog.golang.org/defer-panic-and-recover
 	// https://medium.com/rungo/defer-panic-and-recover-in-go-689dfa7f8802
 
+	// when you use init in go all import variables will be instantiated first
+	// however you dont have controll over what order they will be init
+	// then any init in your main will be instantiated after
 	controllers.RegisterControllers()
 	http.ListenAndServe(":3000", nil)
 }
