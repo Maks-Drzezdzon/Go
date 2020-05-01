06701043b3fc840c2f3b82dbb62debdc5d31302a
maksthorn <maksdrzezdzon@gmail.com>
2020-05-01 18:19:32 +0100

    some more go

---
+++ Leetcode/strings/numJewelsInStones.go
@@ -0,0 +1,10 @@
+import "strings"
+func numJewelsInStones(J string, S string) int {
+    js := strings.Split(J, "")
+    sum := 0
+    for _, j := range js {
+        c  := strings.Count(S, j)
+        sum += c
+    }
+    return sum
+}
