84e642a976065761b45b6d05a867c57e7811d142
maksthorn <maksdrzezdzon@gmail.com>
2020-05-01 18:38:14 +0100

    adding test case

--- Leetcode/strings/numJewelsInStones.go
+++ Leetcode/strings/numJewelsInStones.go
@@ -1,10 +1,13 @@
+package strings
+
 import "strings"
-func numJewelsInStones(J string, S string) int {
-    js := strings.Split(J, "")
-    sum := 0
-    for _, j := range js {
-        c  := strings.Count(S, j)
-        sum += c
-    }
-    return sum
+
+func NumJewelsInStones(J string, S string) int {
+	js := strings.Split(J, "")
+	sum := 0
+	for _, j := range js {
+		c := strings.Count(S, j)
+		sum += c
+	}
+	return sum
 }
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/pluralsight/webservice
-
-go 1.14
-
-require golang.org/x/tools v0.0.0-20200421170005-7504fd22198d
